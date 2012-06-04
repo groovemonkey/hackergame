e1ca3989f1fdfc2a7cd22057eb1cd1ec89a02285
Dave Cohen <dave@project768.com>
2012-06-04 20:08:16 +0200

    added a separate userinput.clj file to handle user-input processing and associated functions

--- src/hacker/core.clj
+++ src/hacker/core.clj
@@ -4,6 +4,7 @@
       hacker.player
       hacker.tasks
       hacker.skills
+      hacker.userinput
       ))
 
 ;;; This is where the main GUI/game loop stuff runs, drawing functions
@@ -54,25 +55,6 @@
   
   )
 
-(defn main-menu-get-player-input
-  "at the main display, after listing player stats, get player input on what s/he wants to do."
-  []
-  ;; what would you like to do?
-
-  ;;(take input: "skills" brings you to skill-view/training-window)
-
-  ;; "charge focus" brings you to the focus-buying screen
-
-  ;; "bank" or "money" brings you to the banking screen
-
-  ;; "view/buy equipment" brings you to the proper equipment screen
-  ;; (list-available-equipment) or (list-player-owned-equipment
-  ;; player) -- somehow also incorporate (view-equipment-item-details)
-  ;; and (buy-equipment item)
-  
-  )
-
-
 
   (defn displaystats
     "Display User Statistics"
@@ -113,7 +95,7 @@
   (loop
       []
     (displaystats (deref player))
-  
+
     ;; take input from player
     (main-menu-get-player-input)
     (if (player-quits)
---
+++ src/hacker/userinput.clj
@@ -0,0 +1,24 @@
+(ns hacker.userinput)
+
+
+
+
+
+
+(defn main-menu-get-player-input
+  "at the main display, after listing player stats, get player input on what s/he wants to do."
+  []
+  ;; what would you like to do?
+
+  ;;(take input: "skills" brings you to skill-view/training-window)
+
+  ;; "charge focus" brings you to the focus-buying screen
+
+  ;; "bank" or "money" brings you to the banking screen
+
+  ;; "view/buy equipment" brings you to the proper equipment screen
+  ;; (list-available-equipment) or (list-player-owned-equipment
+  ;; player) -- somehow also incorporate (view-equipment-item-details)
+  ;; and (buy-equipment item)
+  
+  )
